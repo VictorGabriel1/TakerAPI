7000a00600f4e9d093f3d55fdf2eef61495b8d3e
Victor Gabriel <86345435+VictorGabriel1@users.noreply.github.com>
2021-11-04 18:38:29 -0300

    Update api.py

--- api.py
+++ api.py
@@ -1,30 +1,26 @@
 from flask import Flask
+
 from flask_restful import Resource, Api, reqparse
 import werkzeug
 from taker import taker
 import datetime
 
-app = Flask(__name__,static_folder="output")
+app = Flask(__name__, static_folder="images")
 api = Api(app)
 
+
 class UploadImage(Resource):
-   def post(self):
-     parse = reqparse.RequestParser()
-     parse.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
-     args = parse.parse_args()
+    def post(self):
+        parse = reqparse.RequestParser()
+        parse.add_argument('file', type=werkzeug.datastructures.FileStorage, location='files')
+        args = parse.parse_args()
 
-     print(args)
-    # image_file = args['file']
-    # image_file.save(f"images/{datetime()}.jpg")
+        print(args)
+        image_file = args['file']
+        print('image_file')
+        print(image_file)
 
-    #  response = roda_o_codigo()
-    #  return response
-    #  return {'image_url': 'kajdfhkajhfkshf.jpg', "total": 15}
-
-taker("images/Bob2.png", True)
-
-api.add_resource(UploadImage, '/images/saida.png')
-
-if __name__ == '__main__':
-    app.run(debug=True)
-    
+        response = taker("images/inputs/nova.jpg", True)
+        print(response)
+        image_file.save(f"images/inputs/nova.jpg")
+        return response
